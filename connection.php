6ba253110687ed6f180e7ba325d64aafcf9431a1
PrifoodCorp <61569169+PrifoodCorp@users.noreply.github.com>
2020-03-21 18:29:59 +0800

    Test

--- connection.php
+++ connection.php
@@ -8,11 +8,32 @@ $password = "Admin12345";
 $connectionInfo = array("UID" => "prifood", "pwd" => "Admin12345", "Database" => "DFSI", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 // $serverName = "tcp:stormwebform.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-if($conn) {
-    echo 'Connected';
-} else {
-    // echo 'Error';
-    echo json_encode(sqlsrv_errors($conn));
+// if($conn) {
+//     echo 'Connected';
+// } else {
+//     // echo 'Error';
+//     echo json_encode(sqlsrv_errors($conn));
+// }
+
+
+$stmt = "SELECT *  FROM users WHERE username ='wendy.walburo'";
+$query = sqlsrv_query($conn, $stmt);
+if(!$query) {
+    echo json_encode(sqlsrv_errors($query)); exit();
+} 
+if(sqlsrv_has_rows($query)) {
+    while($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
+        echo $row['username'];
+    }
 }
+
+exit();
+
+
+
+
+
+
+
 exit();
 ?>
