d7f7e3ec1097aa3569b023b22183f29c5243a471
PrifoodCorp <61569169+PrifoodCorp@users.noreply.github.com>
2020-03-21 18:26:18 +0800

    Test

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@
 $serverName  = "stormwebform.database.windows.net"; 
 $user = "prifood";
 $password = "Admin12345"; 
-$connectionInfo = array("UID" => "prifood", "pwd" => "{your_password_here}", "Database" => "DFSI", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "prifood", "pwd" => "Admin12345", "Database" => "DFSI", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 // $serverName = "tcp:stormwebform.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 if($conn) {
